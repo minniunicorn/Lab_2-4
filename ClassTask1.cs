973943bdd05b35efd0af76e6d008f4f866f2662d
minniunicorn <nknknkf92@gmail.com>
2022-02-14 09:31:31 +0300

    Добавьте файлы проекта.

---
+++ ClassTask1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_4
+{
+    class ClassTask1
+    {
+        /// <summary>
+        /// Первое число
+        /// </summary>
+        public string a;
+
+        public static int Numbers(string a)
+        {
+            return a.Length;
+        }
+    }
+}
---
+++ Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab_2_4
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа определения, в каком из двух чисел больше цифр");
+            Console.WriteLine("Введите первое число");
+            string chislo1 = Console.ReadLine();
+            Console.WriteLine("Введите второе число");
+            string chislo2 = Console.ReadLine();
+
+            int numbers1 = ClassTask1.Numbers(chislo1);
+            int number2 = ClassTask1.Numbers(chislo2);
+
+            Console.WriteLine();
+            if (numbers1 > number2)
+            {
+                Console.WriteLine("В первом числе цифр больше");
+            }
+            else
+            {
+                Console.WriteLine("Во втором числе цифр больше");
+            }
+            Console.ReadKey();
+        }
+    }
+}
